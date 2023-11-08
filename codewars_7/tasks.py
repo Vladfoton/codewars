8d7fc7d389186bd18ff0abb8e14ef4c7efb66e40
Vladfoton <vladkuznetcov@mail.ru>
2023-11-08 19:44:37 +0700

    Добавлено решение задачи

---
+++ codewars_7/tasks.py
@@ -0,0 +1,12 @@
+'''Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+'''
+def friend(x:list[str]) -> list[str]:
+    return list(filter(lambda x: len(x) == 4, x))
+    #Code
+
+if __name__ == '__main__':
+    print(friend(["Ryan", "Kieran", "Jason", "Yous"]))
