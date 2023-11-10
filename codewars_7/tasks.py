fa620235b8af4d0a21e4dc610fadbb8900d15e01
Vladfoton <vladkuznetcov@mail.ru>
2023-11-10 12:38:48 +0700

    PEP8 correction in file tasks.py

--- codewars_7/tasks.py
+++ codewars_7/tasks.py
@@ -1,5 +1,9 @@
-'''Make a program that filters a list of strings and returns a list with only your friends name in it.
-If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+import numpy as np
+
+'''
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure
+ he's not...
 Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 '''
 
@@ -10,9 +14,10 @@ def friend(x: list[str]) -> list[str]:
 
 '''
 DESCRIPTION:
-Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
-
-All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including 
+the first one, excluding the last one).
+All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than 
+the first one.
 '''
 
 
@@ -26,8 +31,12 @@ def ips_between(start: str, end: str) -> int:
 
 
 '''
-Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
-Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, 
+he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, 
+check out how contractions are expected to be in the example below.
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from 
+Jaden Smith, but they are not capitalized in the same way he originally typed them.
 Example:
 '''
 
@@ -62,7 +71,8 @@ Use the scoring rules for a standard dartboard:
 
 Finish method:
 def get_score(x,y):
-The coordinates are `(x, y)` are always relative to the center of the board (0, 0). The unit is millimeters. If you throw your dart 5 centimeters to the left and 3 centimeters below, it is written as:
+The coordinates are `(x, y)` are always relative to the center of the board (0, 0). The unit is millimeters. 
+If you throw your dart 5 centimeters to the left and 3 centimeters below, it is written as:
 score = get_score(-50, -30)
 Possible scores are:
 Outside of the board: `"X"`
@@ -71,7 +81,8 @@ Bull: `"SB"`
 A single number, example: `"10"`
 A triple number: `"T10"`
 A double number: `"D10"`
-A throw that ends exactly on the border of two sections results in a bounce out. You can ignore this because all the given coordinates of the tests are within the sections.
+A throw that ends exactly on the border of two sections results in a bounce out. You can ignore this because all 
+the given coordinates of the tests are within the sections.
 The diameters of the circles on the dartboard are:
 Bull's eye: `12.70 mm`
 Bull: `31.8 mm`
@@ -81,15 +92,14 @@ Double ring inner circle: `324 mm`
 Double ring outer circle: `340 mm`
 If you liked this kata, you can continue with: Let's Play Darts: Beat The Power!'''
 
-import numpy as np
-
 
 def get_score(x: int | float, y: int | float):
     sector_data = (6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10)
 
     def get_sector_and_radius(x_coord, y_coord):
         rho = np.sqrt(x_coord ** 2 + y_coord ** 2)
-        phi = np.degrees(np.arctan2(y_coord, x_coord)) if y_coord >= 0 else 360 + np.degrees(np.arctan2(y_coord, x_coord))
+        phi = np.degrees(np.arctan2(y_coord, x_coord)) if y_coord >= 0 else 360 + np.degrees(
+            np.arctan2(y_coord, x_coord))
         return rho, phi
 
     fight_rad, fight_degree = get_sector_and_radius(x, y)
